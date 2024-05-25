3a3304fed99c7619935408458faa6b693a2b7b57
DK DEV <khoacdpp02847@fpt.edu.vn>
2024-05-25 21:31:02 +0700

    khoa
    end 25/5/2024

---
+++ src/main/java/com/webpage/fshop/repository/AccountRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.webpage.fshop.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author 04dkh
+ */
+@Repository
+public interface AccountRepository {
+    
+}
